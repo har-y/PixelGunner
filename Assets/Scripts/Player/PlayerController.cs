75d114acb15917063d014ee751fb669798a05b09
har-y <jakub.harenda@gmail.com>
2021-03-03 23:34:02 +0100

    gun aim to mouse position

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -11,6 +11,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _moveSpeed;
     private Vector2 _moveInput;
 
+    [Header("Player Weapon")]
+    [SerializeField] private Transform _weapon;
+    private Vector3 _mousePosition;
+    private Vector3 _screenPoint;
+    private Vector2 _offset;
+    private float _angle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +28,13 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         PlayerMove();
+
+        _mousePosition = Input.mousePosition;
+        _screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
+
+        _offset = new Vector2(_mousePosition.x - _screenPoint.x, _mousePosition.y - _screenPoint.y);
+        _angle = Mathf.Atan2(_offset.y, _offset.x) * Mathf.Rad2Deg;
+        _weapon.rotation = Quaternion.Euler(0f, 0f, _angle);
     }
 
     private void PlayerMove()
