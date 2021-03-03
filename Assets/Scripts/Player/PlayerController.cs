5411b5edae891e8722887e059edf0e928f8818ee
har-y <jakub.harenda@gmail.com>
2021-03-03 19:53:04 +0100

    moving the player with user input

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Player Movement")]
+    [SerializeField] private float _moveSpeed;
+    private Vector2 _moveInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMove();
+    }
+
+    private void PlayerMove()
+    {
+        _moveInput.x = Input.GetAxisRaw("Horizontal");
+        _moveInput.y = Input.GetAxisRaw("Vertical");
+
+        transform.position += new Vector3(_moveInput.x, _moveInput.y, transform.position.z) * _moveSpeed * Time.deltaTime;
     }
 }
