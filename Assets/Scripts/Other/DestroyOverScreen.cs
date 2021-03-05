74b43a810359dbaf050d25ba2fe15fae96754f17
har-y <jakub.harenda@gmail.com>
2021-03-05 22:50:33 +0100

    destroy bullets after out of screen

--- Assets/Scripts/Other/DestroyOverScreen.cs
+++ Assets/Scripts/Other/DestroyOverScreen.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class DestroyOverScreen : MonoBehaviour
 {
+    private Camera _camera;
+    private Vector2 _screenPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _camera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ScreenBounds();
+    }
+
+    private void ScreenBounds()
+    {
+        _screenPosition = _camera.WorldToScreenPoint(transform.position);
+
+        if (_screenPosition.x > Screen.width || _screenPosition.x < 0 || _screenPosition.y > Screen.height || _screenPosition.y < 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
