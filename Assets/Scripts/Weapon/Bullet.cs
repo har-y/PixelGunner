566af4d7a7e93d67ab351c5f24633fa7afc4de72
har-y <jakub.harenda@gmail.com>
2021-03-08 22:47:31 +0100

    enemy fire enemy bullet

--- Assets/Scripts/Weapon/Bullet.cs
+++ Assets/Scripts/Weapon/Bullet.cs
@@ -6,6 +6,9 @@ public class Bullet : MonoBehaviour
 {
     [Header("Bullet")]
     private Rigidbody2D _rigidbody2D;
+    [SerializeField] private bool _playerBullet;
+    [SerializeField] private bool _enemyBullet;
+
 
     [Header("Bullet - Damage")]
     [SerializeField] private int _bulletDamage;
@@ -16,12 +19,16 @@ public class Bullet : MonoBehaviour
 
     [Header("Bullet - Movement")]
     [SerializeField] private float _moveSpeed;
+    private Vector3 _direction;
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _effectSlot = GameObject.FindGameObjectWithTag("Misc");
+
+        _direction = PlayerController.instance.transform.position - transform.position;
+        _direction.Normalize();
     }
 
     // Update is called once per frame
@@ -32,23 +39,38 @@ public class Bullet : MonoBehaviour
 
     private void BulletMove()
     {
-        _rigidbody2D.velocity = transform.right * _moveSpeed;
+        if (_playerBullet)
+        {
+            _rigidbody2D.velocity = transform.right * _moveSpeed;
+        }
+        else if (_enemyBullet)
+        {
+            transform.position += _direction * _moveSpeed * Time.deltaTime;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         BulletHitObject();
 
-        if (other.tag == "Enemy")
+        if (other.tag == "Enemy" && _playerBullet)
         {
             other.GetComponent<EnemyController>().EnemyDamage(_bulletDamage);
+        }
+
+        if (other.tag == "Player" && _enemyBullet)
+        {
+
         }
     }
 
     private void BulletHitObject()
     {
-        GameObject bulletEffect = Instantiate(_bulletHitEffect, transform.position, transform.rotation);
-        bulletEffect.transform.parent = _effectSlot.transform;
+        if (_playerBullet)
+        {
+            GameObject bulletEffect = Instantiate(_bulletHitEffect, transform.position, transform.rotation);
+            bulletEffect.transform.parent = _effectSlot.transform;
+        }
 
         Destroy(gameObject);
     }
