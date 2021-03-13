67bc95b2c6fdd6e630bf8dbf8bb5a9ae5d904efc
har-y <jakub.harenda@gmail.com>
2021-03-13 22:41:39 +0100

    broken piece of broken box

--- Assets/Scripts/Other/Breakables.cs
+++ Assets/Scripts/Other/Breakables.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Breakables : MonoBehaviour
 {
+    [Header("Breakable")]
+    [SerializeField] private GameObject[] _brokenPieces;
+    private GameObject[] _brokenPiece;
+    private GameObject _pieceSlot;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _pieceSlot = GameObject.FindGameObjectWithTag("Misc");
+        _brokenPiece = new GameObject[_brokenPieces.Length];
     }
 
     // Update is called once per frame
@@ -23,6 +29,13 @@ public class Breakables : MonoBehaviour
             if (PlayerController.instance.DashCounter > 0)
             {
                 Destroy(gameObject);
+
+                for (int i = 0; i < _brokenPieces.Length; i++)
+                {
+                    GameObject piece = Instantiate(_brokenPieces[i], transform.position, transform.rotation);
+                    _brokenPiece[i] = piece;
+                    _brokenPiece[i].transform.parent = _pieceSlot.transform;
+                }
             }
         }
     }
