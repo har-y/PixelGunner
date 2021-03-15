14beb742bea71b609c29e8e742bd878f854b29b4
har-y <jakub.harenda@gmail.com>
2021-03-15 19:42:18 +0100

    close room doors after player enter the room

--- Assets/Scripts/Controller/CameraController.cs
+++ Assets/Scripts/Controller/CameraController.cs
@@ -10,7 +10,6 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float _moveSpeed;
     [SerializeField] private Transform _target;
 
-
     private void Awake()
     {
         instance = this;
--- Assets/Scripts/Level/Room/GridRoom.cs
+++ Assets/Scripts/Level/Room/GridRoom.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class GridRoom : MonoBehaviour
 {
+    [Header("Room")]
+    [SerializeField] private bool _firstRoom;
+    [SerializeField] private bool _enterCloseDoors;
+    [SerializeField] private GameObject[] _doors;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        FirstRoom();
     }
 
     // Update is called once per frame
@@ -21,6 +27,33 @@ public class GridRoom : MonoBehaviour
         if (other.tag == "Player")
         {
             CameraController.instance.ChangeTarget(transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            CloseRoomDoor();
+        }
+    }
+
+    private void CloseRoomDoor()
+    {
+        if (_enterCloseDoors)
+        {
+            foreach (GameObject item in _doors)
+            {
+                item.SetActive(true);
+            }
+        }
+    }
+
+    private void FirstRoom()
+    {
+        if (_firstRoom)
+        {
+            CloseRoomDoor();
         }
     }
 }
