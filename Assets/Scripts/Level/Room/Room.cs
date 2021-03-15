f7c4e124b128f7dbeeee19640ae673d4d57b6409
har-y <jakub.harenda@gmail.com>
2021-03-15 20:28:11 +0100

    clear a room

---
+++ Assets/Scripts/Level/Room/Room.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    [Header("Room")]
+    [SerializeField] private bool _firstRoom;
+    [SerializeField] private bool _closeDoorsEnter;
+    [SerializeField] private bool _openDoorsEnemy;
+    private bool _activeRoom;
+
+    [Header("Room - Doors")]
+    [SerializeField] private GameObject[] _doors;
+
+    [Header("Room - Enemy")]
+    [SerializeField] private List<GameObject> _enemy = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirstRoom();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_activeRoom && _openDoorsEnemy && _enemy.Count > 0)
+        {
+            for (int i = 0; i < _enemy.Count; i++)
+            {
+                if (_enemy[i] == null)
+                {
+                    _enemy.RemoveAt(i);
+
+                    i--;
+                }
+            }
+
+            if (_enemy.Count == 0)
+            {
+                OpenRoomDoor();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            CameraController.instance.ChangeTarget(transform);
+
+            _activeRoom = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            CloseRoomDoor();
+
+            _activeRoom = true;
+        }
+    }
+
+    private void CloseRoomDoor()
+    {
+        if (_closeDoorsEnter)
+        {
+            foreach (GameObject item in _doors)
+            {
+                item.SetActive(true);
+            }
+        }
+    }
+
+    private void OpenRoomDoor()
+    {
+        foreach (GameObject item in _doors)
+        {
+            item.SetActive(false);
+
+            _closeDoorsEnter = false;
+        }
+    }
+
+    private void FirstRoom()
+    {
+        if (_firstRoom)
+        {
+            CloseRoomDoor();
+
+            _activeRoom = true;
+        }
+    }
+}
