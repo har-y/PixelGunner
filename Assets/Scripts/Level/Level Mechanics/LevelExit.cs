3126c366179792de4bbb0d93bf84c5d5ead3a4f1
har-y <jakub.harenda@gmail.com>
2021-03-17 00:26:38 +0100

    exit the level after player collide with level exit object

--- Assets/Scripts/Level/Level Mechanics/LevelExit.cs
+++ Assets/Scripts/Level/Level Mechanics/LevelExit.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelExit : MonoBehaviour
 {
+    [Header("Level Exit")]
+    [SerializeField] private string _load;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,13 @@ public class LevelExit : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(_load);
+        }
     }
 }
