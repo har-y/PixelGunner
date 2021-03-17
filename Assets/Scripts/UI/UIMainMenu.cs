a4ea045024e0f79768698ea7aefa4bfb8c1c93d4
har-y <jakub.harenda@gmail.com>
2021-03-17 19:17:30 +0100

    active start game and exit game button

--- Assets/Scripts/UI/UIMainMenu.cs
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIMainMenu : MonoBehaviour
 {
+    [Header("UI Main Menu")]
+    [SerializeField] private string _levelLoad;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,15 @@ public class UIMainMenu : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(_levelLoad);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
