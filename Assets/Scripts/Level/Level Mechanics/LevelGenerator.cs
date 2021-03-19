0353d2478647e24d43c27be030d92ff8fcee1672
har-y <jakub.harenda@gmail.com>
2021-03-19 20:17:44 +0100

    generate more rooms

--- Assets/Scripts/Level/Level Mechanics/LevelGenerator.cs
+++ Assets/Scripts/Level/Level Mechanics/LevelGenerator.cs
@@ -10,12 +10,11 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] private Color _startColor;
     [SerializeField] private Color _endColor;
 
-    [Header("Level Generator")]
-
-    [SerializeField] private int _layoutSpace;
+    [Header("Level Generator - Room")]
     [SerializeField] private Direction _direction;
     [SerializeField] private float _xOffset = 18f;
     [SerializeField] private float _yOffset = 10f;
+    [SerializeField] private int _roomValue;
 
     private enum Direction
     {
@@ -28,8 +27,29 @@ public class LevelGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        StartRoomGenerator();
+
+        for (int i = 0; i < _roomValue; i++)
+        {
+            RoomGenerator();
+        }
+    }
+
+    private void StartRoomGenerator()
+    {
         Instantiate(_layoutRoom, _generatorPoint.position, _generatorPoint.rotation).GetComponent<SpriteRenderer>().color = _startColor;
-        _direction = (Direction) Random.Range(0, 4);
+        RandomRandomDirection();
+    }
+
+    private void RoomGenerator()
+    {
+        Instantiate(_layoutRoom, _generatorPoint.position, _generatorPoint.rotation);
+        RandomRandomDirection();
+    }
+
+    private void RandomRandomDirection()
+    {
+        _direction = (Direction)Random.Range(0, 4);
         GeneratorPointMove();
     }
 
