e2e15a97acd0a93efcf8d71ddf8546c0fe1df756
har-y <jakub.harenda@gmail.com>
2021-03-19 19:43:20 +0100

    appoint a start room

--- Assets/Scripts/Level/Level Mechanics/LevelGenerator.cs
+++ Assets/Scripts/Level/Level Mechanics/LevelGenerator.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+    [Header("Level Generator")]
+    [SerializeField] private GameObject _layoutRoom;
+    [SerializeField] private int _layoutSpace;
+
+    [SerializeField] private Color _startColor;
+    [SerializeField] private Color _endColor;
+
+    [SerializeField] private Transform _generatorPoint;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(_layoutRoom, _generatorPoint.position, _generatorPoint.rotation).GetComponent<SpriteRenderer>().color = _startColor;
     }
 
     // Update is called once per frame
